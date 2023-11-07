3e2404d29ce62a5530585aaa2583ac1122c02453
joy777park <joy777park@gmail.com>
2023-11-07 14:07:03 +0800

    輸入 10 個數字 降冪

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Linq;
+
 Console.WriteLine("Hello, World!");
+
+List<int> ints = new List<int>();
+int i;
+
+do
+{
+    Console.WriteLine("輸入數字");
+    var x = Console.ReadLine();
+    if (int.TryParse(x, out i))
+    {
+        ints.Add(i);
+    }
+} while (ints.Count != 10);
+
+Console.WriteLine(string.Join(", ", ints.OrderByDescending(t => t)));
+
+
+Console.ReadLine();
