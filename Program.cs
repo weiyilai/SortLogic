1d0549ec66d5becde51181863ebca3352a70afb6
joy777park <joy777park@gmail.com>
2023-11-07 18:01:52 +0800

    檢核 不重複輸入的數字

--- Program.cs
+++ Program.cs
@@ -10,10 +10,16 @@ do
 {
     Console.WriteLine("輸入數字");
     var x = Console.ReadLine();
-    if (int.TryParse(x, out i))
+    bool num = int.TryParse(x, out i);
+    if (ints.Exists(t => t == i))
+    {
+        Console.WriteLine("請輸入不重複的數字");
+    }
+    else
     {
         ints.Add(i);
     }
+
 } while (ints.Count != 10);
 
 Console.WriteLine(string.Join(", ", ints.OrderByDescending(t => t)));
